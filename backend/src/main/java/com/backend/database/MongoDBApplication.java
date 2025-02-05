951aa9f1848b389ac7afc2104c42e9b434004636
Jimmykhangnguyen <jimmykhangnguyen@gmail.com>
2025-02-05 15:45:19 -0500

    Set up and Connected the backend to MongoDB

--- backend/src/main/java/com/backend/database/MongoDBApplication.java
+++ backend/src/main/java/com/backend/database/MongoDBApplication.java
@@ -1,13 +1,13 @@
-package com.backend.backend;
+package com.backend.database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class MongoDBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(MongoDBApplication.class, args);
 	}
 
 }

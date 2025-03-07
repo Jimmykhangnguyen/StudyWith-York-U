e089dcf359ecd5b35d8b0eeee4d6c6ea248b81d3
bonilla-kimberly <kim29@my.yorku.ca>
2025-03-07 00:50:08 -0500

    added front end of login page including the login.component.html and login.component.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { MapComponent } from './map/map.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
     {path: 'map', component: MapComponent}
 ];
+
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Import FormsModule for ngModel
+import { Router } from '@angular/router';  // Import Router to make page transitions
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  loginError: boolean = false;
 
+  //use a router to make transitions between pages
+  constructor(private router: Router) {}
+
+  // When login button is clicked move to 'map'
+  onSubmit() {
+    // For demo, assume login is always successful to redirect to the map
+    this.router.navigate(['/map']);  
+  }
 }

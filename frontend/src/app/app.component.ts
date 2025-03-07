7cc9dd441dc6f7256399f42ee6cee4058d3424d9
Jimmykhangnguyen <jimmykhangnguyen@gmail.com>
2025-03-07 15:25:59 -0500

    Getting Log In to display

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MapComponent } from './map/map.component';
 import { StudyAreaComponent } from './study-area/study-area.component';
+import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http'; // Add this import
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MapComponent, StudyAreaComponent, HttpClientModule],
+  imports: [RouterOutlet, MapComponent, LoginComponent, StudyAreaComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

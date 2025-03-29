61acf844bf992c85e6435aeddccaaad57d9913d7
JMM-23 <jmm23@my.yorku.ca>
2025-03-29 01:03:25 -0400

    Setup for pages

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -21,7 +21,7 @@ export class MapComponent implements OnInit {
     ['end', []]
   ]);
   pointColours = new Map<string, string>([
-    ['start', '#4a0000'],
+    ['start', '#3887be'],
     ['end', '#e53e3e']
   ]);
 

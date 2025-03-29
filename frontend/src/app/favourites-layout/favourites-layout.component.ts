5af0abf2c460a55faef74e677fe130924f8433ce
JMM-23 <jmm23@my.yorku.ca>
2025-03-29 01:03:25 -0400

    Setup for pages

---
+++ frontend/src/app/favourites-layout/favourites-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-favourites-layout',
+  standalone: true,
+  imports: [NavBarComponent],
+  templateUrl: './favourites-layout.component.html',
+  styleUrl: './favourites-layout.component.css'
+})
+
+export class FavouritesLayoutComponent {
+
+}
